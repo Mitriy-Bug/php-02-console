76468ea5436dae9f93b9e3e54ee53575d99af3bf
Mitriy_Bug <verstkajoom@gmail.com>
2024-11-28 22:27:34 +0300

    php-02 add do while

--- index.php
+++ index.php
@@ -1,25 +1,15 @@
 <?php
-echo "Введите делимое: ";
-echo $enteredNumbers1 = fgets(STDIN);
-$divisible = trim($enteredNumbers1); //Делимое
-
-if (is_int($divisible)!==true){
-    echo "Введите целое число". PHP_EOL;
+do {
     echo "Введите делимое: ";
-    $enteredNumbers1 = fgets(STDIN);
+    echo $enteredNumbers1 = fgets(STDIN);
     $divisible = trim($enteredNumbers1); //Делимое
-}
+} while (is_numeric($divisible)!==true);
 
-echo "Введите делитель: ";
-$enteredNumbers2 = fgets(STDIN);
-$divider = trim($enteredNumbers2); //Делитель
-
-if (is_int($divider)!==true){
-    echo "Введите целое число". PHP_EOL;
+do {
     echo "Введите делитель: ";
     $enteredNumbers2 = fgets(STDIN);
     $divider = trim($enteredNumbers2); //Делитель
-}
+} while (is_numeric($divider)!==true);
 
 if ($divider === "0") {//проверяем, что делитель не равен "0"
     echo "Делить на ноль нельзя" . PHP_EOL;
